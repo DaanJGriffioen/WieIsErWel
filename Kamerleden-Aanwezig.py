235be07728809e55a875c1028c411900341bf2eb
DaanJGriffioen <daangrif@gmail.com>
2024-10-18 00:00:44 +0200

    Git functionaliteit toegevoegd

--- Kamerleden-Aanwezig.py
+++ Kamerleden-Aanwezig.py
@@ -1,4 +1,5 @@
 import argparse
+import string
 import pandas as pd
 import numpy as np
 import array
@@ -14,6 +15,7 @@ from datetime import date, datetime, timedelta
 import argparse
 
 debug = False
+git = False
 
 def kamerleden():
   namen = ["Jong", "Bosma", "Dijck", "Dijk", "Groot", "Jansen", "Vries", "Mulder"]
@@ -207,13 +209,14 @@ def arrayParsing(aanwezig, afwezig):
   return df
 
 # Make nice graph who is present and who is not
-def makeHTML(aanwezig, afwezig):
+def makeHTML(aanwezig, afwezig, datums):
   data = arrayParsing(aanwezig, afwezig)
   
   data = data["afwezig"].value_counts()
   f = open("index.html", "w")
   f.write("<!DOCTYPE html>\n<html>\n<head>\n<link rel=\"stylesheet\" href=\"style.css\">\n</head>\n<body>")
   f.write("<h1>\nKamerleden Afnwezigheid</h1>\n")
+  f.write(f"<h2>Afwezigheid van {datums[0]} tot {datums[1]}</h2>")
   f.write("<table>\n")
   f.write("<th>Naam</th><th>Aantal</th>")
   for entry in data.items():
@@ -249,11 +252,11 @@ def aanwezigheid(datum):
   aanwezig, afwezig = presentie(kamerleden)
   f = open(f"files/logs/log_{datum}.txt", "a")
   f.write("Aanwezig:\n")
-  for str in aanwezig:
-    f.write(str + '\n')
+  for stri in aanwezig:
+    f.write(stri + '\n')
   f.write("\nAfwezig:\n")
-  for str in afwezig:
-    f.write(str + '\n')
+  for stri in afwezig:
+    f.write(stri + '\n')
   f.close()
 
   return aanwezig, afwezig
@@ -261,25 +264,36 @@ def aanwezigheid(datum):
 
 def main():
   bereik = False
-  str = input("1: Zelf datum opgeven \n2: Bereik van data: ")
+  data = []
   aanwezig = ""
   aanwezig_arr = []
   afwezig_arr = []
   #  Een specifieke datum
-  if int(str) == 1:
-    str = input("Geef een datum op (YYYY-MM-DD): ")
+  if git:
+    datum = date.today() - timedelta(days=1)
+    stri = str(datum)
+    aanwezig, afwezig = aanwezigheid(datum)
+    data.extend([stri, stri])
+    makeHTML(aanwezig, afwezig, data)
+    return
+
+  stri = input("1: Zelf datum opgeven \n2: Bereik van data:\n")
+  if int(stri) == 1:
+    stri = input("Geef een datum op (YYYY-MM-DD): ")
     # Check de invoer voor yyyy-mm-dd
-    assert str.split('-')[0].__len__() == 4
-    assert str.split('-')[1].__len__() == 2
-    assert str.split('-')[2].__len__() == 2
-    datum = date.fromisoformat(str)
+    assert stri.split('-')[0].__len__() == 4
+    assert stri.split('-')[1].__len__() == 2
+    assert stri.split('-')[2].__len__() == 2
+    datum = date.fromisoformat(stri)
     if date.today() < datum:
-      print("Kan niet in de toekomst kijken, misschien een volgende update :)")
+      print("Kan niet in de toekomst kijken, misschien een volgende update ;)")
       exit(0)
     aanwezig, afwezig = aanwezigheid(datum)
+    data.extend([stri, stri])
 
   # Bereik van data
-  elif int(str) == 2:
+  elif int(stri) == 2:
+    data = []
     bereik = True
     str1 = input("Geef een eerste datum op (YYYY-MM-DD): ")
     str2 = input("Geef een tweede datum op (YYYY-MM-DD): ")
@@ -293,13 +307,17 @@ def main():
     assert str2.split('-')[1].__len__() == 2
     assert str2.split('-')[2].__len__() == 2
     datum2 = date.fromisoformat(str2)
+    data.extend([str1, str2])
     
+
     if date.today() < datum2:
       print("Kan niet in de toekomst kijken, misschien een volgende update :)")
       exit(0)
 
+    # Bereken het verschil tussen de datums
     delta = datum2 - datum1
-    
+
+    # Op zaterdagen en zondagen wordt er niet gedebatteerd
     for _ in range(delta.days):
       datum1 += timedelta(days=1)
       if datum1.isoweekday == 6 or datum1.isoweekday == 7:
@@ -320,13 +338,14 @@ def main():
   bezig = True
   
   # Wachten op antwoord waar we iets mee kunnen
+    
   while bezig:
-    str = input("HTML maken? j/n: \n")
-    if str == "j" or str == "J":
-      if bereik: makeHTML(aanwezig_arr, afwezig_arr)
-      else: makeHTML(aanwezig, afwezig)
+    stri = input("HTML maken? j/n: \n")
+    if stri == "j" or stri == "J":
+      if bereik: makeHTML(aanwezig_arr, afwezig_arr, data)
+      else: makeHTML(aanwezig, afwezig, data)
       bezig = False
-    elif str == "n" or str == "N":
+    elif stri == "n" or stri == "N":
       bezig = False
     else:
       print("Invoer is j/n")
@@ -337,7 +356,11 @@ if __name__=="__main__":
   parser.add_argument("--debug", default=False, type=bool, metavar=debug,
                       help="Set debug to True if you want to see the output of\
                             the getting and parsing process")
+  parser.add_argument("--git", default=False, type=bool, metavar=debug,
+                      help="Used when run on github pages to make nice table")
   args = parser.parse_args()
   debug = args.debug
+  git = args.git
+
   kamerleden()
   main()
